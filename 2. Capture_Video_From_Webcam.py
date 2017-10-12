cbd8b16953321581172ab761f6999d8193a9f0c7
frincyc <32052223+frincyc@users.noreply.github.com>
2017-10-11 22:35:08 -0600

    Update 2. Capture_Video_From_Webcam.py

--- 2. Capture_Video_From_Webcam.py
+++ 2. Capture_Video_From_Webcam.py
@@ -2,16 +2,16 @@ import numpy as np
 import cv2
 capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out =  cv2.VideoWriter('Webcam_Output.avi',fourcc, 20.0,(1280,960))
+video_out =  cv2.VideoWriter('Webcam_Output.avi',fourcc, 20.0,(1280,960))
 while(capture.isOpened()):
     ret,frame=capture.read()
     if ret == True:
-        out.write(frame)
+        video_out.write(frame)
         cv2.imshow('frame',frame)
         if cv2.waitKey() == 27: #Press Esc key to exit recording
             break
     else:
         break
 capture.release()
-out.release()
+video_out.release()
 cv2.destroyAllWindows()
